a84265ff5f31b90283572b3284dcde666d0aeff9
Anonymous <Sunconure11@users.noreply.github.com>
2021-03-23 11:11:21 -0400

    Update BewitchmentPlus.java

--- src/main/java/net/bewitchmentplus/BewitchmentPlus.java
+++ src/main/java/net/bewitchmentplus/BewitchmentPlus.java
@@ -33,16 +33,5 @@ public class BewitchmentPlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		logger.info("Remember when I told you how my");
-		logger.info("Kin is different in some ways?");
-
-		logger.info("It's a fact, she is exactly that!");
-		logger.info("A harbinger of death from the world of witchcraft,");
-		logger.info("And she's feeding them cakes and her ale to this innocent boy,");
-		logger.info("And her magic brings dismay!");
-
-		logger.info("I hear her in the wind, the bane of our town");
-		logger.info("Come with me, father, I'm to expose a heathen");
 	}
 }
