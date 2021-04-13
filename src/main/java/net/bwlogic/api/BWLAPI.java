da090da8c46ee619c9fb9dbf9d0dfaf5926e2839
Sunconure11 <eyespot7b@aol.com>
2021-04-12 23:55:29 -0400

    Working on fixing this first

--- src/main/java/net/bwlogic/api/BWLAPI.java
+++ src/main/java/net/bwlogic/api/BWLAPI.java
@@ -2,7 +2,7 @@ package net.bwlogic.api;
 
 import net.minecraft.entity.EntityGroup;
 
-public class BWPAPI {
+public class BWLAPI {
 
 	@SuppressWarnings("InstantiationOfUtilityClass")
 	public static final EntityGroup ANGEL = new EntityGroup();
